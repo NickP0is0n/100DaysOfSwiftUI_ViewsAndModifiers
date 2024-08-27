e4a02798288cdc2cc00569192b4579b3fedcd082
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-27 14:42:30 +0300

    Expanded VStack to fill whole screen

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -14,8 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .foregroundStyle(.white)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.purple)
     }
 }
 
