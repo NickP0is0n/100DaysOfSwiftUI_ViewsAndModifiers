5302c757394ea8115c2cf2525337ad49fb0e1276
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-27 14:50:59 +0300

    Modifier order demo

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -15,6 +15,20 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
                 .foregroundStyle(.white)
+            Button("Hello, world!") {
+                // Do nothing
+            }
+            .frame(width: 200, height: 200)
+            .background(.red)
+            Text("I have many mods!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.yellow)
+                .padding()
+                .background(.green)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.purple)
